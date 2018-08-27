e7c79d2f996b7fc2bb6d1e87705f7c023842bb60
piovittoriovigo <bserv3425@gmail.com>
2018-08-27 18:07:47 +0000

    listen backlog limited to 100 connections

--- flask_app.py
+++ flask_app.py
@@ -6,13 +6,18 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
     username="piovittoriovigo",
     password="Exeter22",
     hostname="piovittoriovigo.mysql.pythonanywhere-services.com",
-    databasename="piovittoriovigo",
+    databasename="piovittoriovigo$comments",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+class Comment(db.Model):
 
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 comments = []
 
 @app.route("/", methods=["GET", "POST"])
